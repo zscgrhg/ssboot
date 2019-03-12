65d7ea896416deaa0d5f4db426bc16c852a8a82a
zscgrhg <zscgrhg@qq.com>
2019-03-12 21:36:53 +0800

    a

--- src/main/java/com/example/demo/action/AdminController.java
+++ src/main/java/com/example/demo/action/AdminController.java
@@ -4,12 +4,20 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @Secured("ROLE_ADMIN")
 public class AdminController {
 
-    @RequestMapping({"/admin", "/admin/index"})
+    @RequestMapping({"/admin/index"})
     public String pub() {
         return "admin/index";
+    }
+
+    @RequestMapping({"/admin/logout"})
+    public String logout(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "redirect:/index";
     }
 }
--- src/main/java/com/example/demo/ctx/SecurityConfig.java
+++ src/main/java/com/example/demo/ctx/SecurityConfig.java
@@ -3,34 +3,27 @@ package com.example.demo.ctx;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.security.SecureRandom;
-@EnableWebSecurity
+
 @Configuration
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf()
                 .disable()
                 .authorizeRequests()
-                // .mvcMatchers("/","/index","/public","/public/index")
-                .antMatchers("/*", "/public/**")
-                .permitAll()
+                .antMatchers("/admin/**").authenticated()
                 .and()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                //.httpBasic()
                 .formLogin()
-                .defaultSuccessUrl("/admin")
+                .defaultSuccessUrl("/admin/index")
+                .and().exceptionHandling().accessDeniedPage("/403.html")
                 .and()
-                .logout().logoutSuccessUrl("/public")
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
     }
 
     @Override
@@ -39,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(new BCryptPasswordEncoder(10, new SecureRandom()))
                 .withUser("admin")
                 .password("$2a$10$vzZzEX6HXyphUZEXoADVxeWIROWBA8aevSwelG2HyRLzO4N6gEN8S")
-                .roles("ADMIN")
+                .roles("USER")
                 .and()
                 .withUser("manager")
                 .password("$2a$10$vzZzEX6HXyphUZEXoADVxeWIROWBA8aevSwelG2HyRLzO4N6gEN8S")
